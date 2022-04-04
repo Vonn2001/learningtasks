ee8cc152b0f6a622dc641dc32ed343c400c00030
Vonn2001 <100071064+Vonn2001@users.noreply.github.com>
2022-04-04 19:08:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 <h1>Welcome To My Website! I Am Von!</h1>
 <ul>
 <li><a href="index.php">Home</a></li>
-<li><a href="file:///C:/Users/ASUS/Documents/HTML/LT2/LT2.html">Learning task 2</a></li>
-<li><a href="file:///C:/Users/ASUS/Documents/HTML/LT3/LT3.html">Learning task 3</a></li>
-<li><a href="file:///C:/Users/ASUS/Documents/HTML/CLE1MT/CLE1MT.html">CLE1MT</a></li>
+<li><a href="LT2.html">Learning task 2</a></li>
+<li><a href="LT3.html">Learning task 3</a></li>
+<li><a href="CLE1MT.html">CLE1MT</a></li>
 </ul>
 </body>
 </html>

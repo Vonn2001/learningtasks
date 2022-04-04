c7c940394724e783e87bd6c0a6464ea3f4cc3f93
Vonn2001 <100071064+Vonn2001@users.noreply.github.com>
2022-04-04 18:58:52 +0800

    Add files via upload

---
+++ CLE1MT/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> My Home Page </title>
+</head>
+<body>
+<h1>Poverty!</h1>
+<h3>The Philippines has a fairly high poverty rate with more than 16% of the population living below the poverty line. Because of the many people reliant on agriculture for an income and inequality in wealth distribution, about 17.6 million Filipinos struggle to afford basic necessities. From 2015 to 2020, the rate of poverty declined from 21.6% to 16.6%. Philippine President Rodrigo Duterte aims to reduce the rate of poverty to 14% by 2022. Through its strategy, AmBisyon 2040, the Philippine government plans to eradicate extreme poverty by 2040. Furthermore, the government has implemented various programs and reforms to reduce poverty by targeting education, healthcare and the overall economy. Here are five ways the program is combating poverty in the Philippines.</h3>
+</body>
+</html>
